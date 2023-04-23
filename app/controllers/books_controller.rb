f51885198d3cb1b05e5a4ef41e7ac8aa44daaf93
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-04-23 19:12:05 +0000

    activestorage install前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,18 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   def create
-    @book = Book.new(book_image_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_images_path
+    redirect_to books_path
   end
+  
   def show
+  end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
@@ -16,7 +22,8 @@ class BooksController < ApplicationController
   
   private
 
-  def post_image_params
-    params.require(:book_image).permit(:name, :image, :opinion)
+  def book_params
+    params.require(:book).permit(:user_id, :image, :body, :title)
+    
   end
 end
