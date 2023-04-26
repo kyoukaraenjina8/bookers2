a1338dcd31c2fc31efbecc2a67876fd8282936a0
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-04-26 13:14:18 +0000

    process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+      @user = User.find(params[:id])
   end
   def update
-    @user = User.new(user_params)
-    @user.save
+    @user = User.find(params[:id])
+    @user.update(user_params)
     redirect_to users_path(@user.id)
   end
 
   def index
     @user = User.all
   end
+
+  private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about"
   devise_for :users
+  root to: "homes#top"
+  get "/home/about" => "homes#about", as: "about"
   resources :books, only: [:show, :edit, :create, :index, :new ]
   resources :users, only: [:show, :edit, :create, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
